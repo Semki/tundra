000b7a6a48cbf1ce07423035a136bcb64188793a
cheredon <cheredon@gmail.com>
2012-02-12 18:09:08 +0800

    Schema

--- app/controllers/Schema.java
+++ app/controllers/Schema.java
@@ -12,7 +12,14 @@ public class Schema extends Controller {
 	}
 	
 	public static void create(JsonObject body) {
-		
+		if (body == null){
+			System.out.println("trololo");
+		}
+		renderJSON(body);
+	}
+	
+	public static void read(){
+		System.out.println();
 		renderJSON("[]");
 	}
 
