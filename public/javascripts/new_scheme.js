e9ff10043e6fd6b318d6c4e1fead5aaf3130e386
Anton Zhavoronkov <djzhan@list.ru>
2012-02-14 22:13:09 +0800

    tables constructor

--- public/javascripts/new_scheme.js
+++ public/javascripts/new_scheme.js
@@ -1,37 +1,55 @@
 $(document).ready(function() {
 	
-	var result = {project_id: "", tables: []};
+	var result = {project_id: "", user_id: "kvitunov", tables: []};
 	var tables = {};
+	var tablesCount = 0;
 	
 	var addTable = function (tableName) {
 		result.tables.push({table_name: tableName, columns: []});
 		//alert(JSON.stringify(result));
 		var id = result.tables.length - 1;
 		
-		$('#tables_wrapper').append('<div id="' + id + '" class="table_wrapper"><h3 class="table_header">' + tableName + '</h3>' + 
-				                    '<table><thead><tr><th>Column name</th><th>Data type</th></tr></thead></table>' +
-				                    '<input class="column_name col_3" id="' + id + '" type="text" placeholder="Column name" />' +
+		$('#tables_wrapper').append('<div id="' + id + '" class="table_wrapper"><h3 class="table_header" id="table_name'+id+'">' + tableName + '</h3>' + 
+				                    '<table id="table_body' + id + '"><thead><tr><th>Column name</th><th>Data type</th><th></th></tr></thead></table>' +
+				                    '<input id="column_name_tb' + id + '" class="col_3" id="' + id + '" type="text" placeholder="Column name" />' +
 				                    '<button class="small add_column" id="' + id + '"><span class="icon"><span aria-hidden="true">+</span></span>Add column</button></div>');
 		
 		
 		
+		$("#actions_wrapper").empty();
+		$("#actions_wrapper").append('<br/><button class="medium green" id="generate_scheme"><span class="icon"><span aria-hidden="true">F</span></span>Generate scheme</button>')
 	};
+	
+	$('#generate_scheme').live('click', function () {
+		for (var i = 0; i < tablesCount; ++i) {
+			$.each($(".column_name"+i), function() {
+				result.tables[i].columns.push({column_name: this.textContent, type: "string"});
+			});
+		}
+		
+		alert(JSON.stringify(result));
+	});
 	 
 	$('button.add_column').live('click', function () {
-		var columnName = $('.column_name#' + this.id).val();
-		alert(columnName);
-		/*
-		if (tableName == "") {
-			$('#table_name').addClass("error");
+		var tableId = this.id;
+		var columnNameInput = $('#column_name_tb' + this.id);
+		var columnName = columnNameInput.val();
+		
+		if (columnName == "") {
+			columnNameInput.addClass("error");
 		}
 		else {
-			$('#table_name').removeClass("error");
-			$('#table_name').val('');
+			columnNameInput.removeClass("error");
+			columnNameInput.val('');
 			
-			addTable(tableName);
+			addColumnToTable(tableId, columnName);
 		}
-		*/
+
 	});
+	
+	var addColumnToTable = function (tableId, columnName) {
+		$("#table_body" + tableId).append('<tr><td class="column_name'+tableId+'">' + columnName + '</td><td class="column_datatype'+tableId+'">String</td><td><span class="icon gray small"><span aria-hidden="true">x</span></span></td></tr>');
+	};
 	
 	$('button').unbind("click");
 	$('#create_table').click(function () {
@@ -45,6 +63,7 @@ $(document).ready(function() {
 			$('#table_name').val('');
 			
 			addTable(tableName);
+			++tablesCount;
 		}
 	});
 });
