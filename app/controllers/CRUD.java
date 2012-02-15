0b1038c42753cd650fb1b4aa3456f0287f3df25d
cheredon <cheredon@gmail.com>
2012-02-15 16:03:49 +0800

    get projects to list

--- app/controllers/CRUD.java
+++ app/controllers/CRUD.java
@@ -33,8 +33,8 @@ public class CRUD extends BaseController{
 			condition.FieldName = fieldName;
 			
 			SortCondition sort = new SortCondition();
-			sort.fieldName = fieldName;
-			sort.fieldType = SchemaManager.Instance().GetFieldType(condition.ProjectId, condition.TableName, condition.FieldName);
+			//sort.fieldName = fieldName;
+			//sort.fieldType = SchemaManager.Instance().GetFieldType(condition.ProjectId, condition.TableName, condition.FieldName);
 		
 			ArrayList<JsonObject> result = listWorker.GetList(condition, sort, null);
 
