a58d5f45568b761b6dc56eed7fded3ebb87f86ff
Anton Zhavoronkov <djzhan@list.ru>
2012-02-15 15:42:23 +0800

    proj list

--- public/javascripts/projects_list.js
+++ public/javascripts/projects_list.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-	alert("ololo");
 
 	$.get("/project/get_projects_list", function (data) {
-		$.each(data, function () {
-			$("#projects_list").append("<li><span>" + this.project_name + "</span>" + "<h3>" + this.project_id + "</h3>" + "</li>");
-		});
+		if (data.elements.length != 0) {
+			$.each(data.elements, function () {
+				$("#projects_list").append("<li><span>" + this.project_name + "</span>" + "<h3>" + this.project_id + "</h3>" + "</li>");
+			});
+		}
+		else {
+			$("#projects_list").append("<h2>No projects created yet...</h2>");
+		}
 	});
 	
 });
